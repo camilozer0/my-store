f08120298f318eb13a852831fdb86a0c5fb76470
Camilo Mazorra <mazorra006@gmail.com>
2023-06-13 18:49:00 -0500

    add and remove from cart

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -39,11 +39,28 @@ export class CartComponent implements OnInit {
   constructor(private cartService: CartService) { }
 
   ngOnInit(): void {
+    this.cartService.cart.subscribe((_cart: Cart) => this.cart = _cart);
     this.dataSource = this.cart.items;
   }
 
   getTotal(items: Array<CartItem>): number {
     return this.cartService.getTotal(items);
+  }
+
+  onClearCart(): void {
+    this.cartService.clearCart();
+  }
+
+  onRemoveFromCart(item: CartItem): void {
+    this.cartService.removeFromCart(item);
+  }
+
+  onAddQuantity(item: CartItem): void {
+    this.cartService.addToCart(item);
+  }
+
+  onRemoveQuantity(item: CartItem): void {
+    this.cartService.removeQuantity(item);
   }
 
 }
