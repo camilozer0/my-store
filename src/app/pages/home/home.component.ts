c68c1c769f5c84f6eb79140fb1431457f8eb69f0
Camilo Mazorra <mazorra006@gmail.com>
2023-06-17 12:33:22 -0500

    minor fix

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -45,12 +45,12 @@ export class HomeComponent implements OnInit, OnDestroy {
 
   onItemsCountChange(newCount: number): void {
     this.count = newCount.toString();
-    this.getProducts;
+    this.getProducts();
   }
 
   onSortChange(newSort: string): void {
     this.sort = newSort;
-    this.getProducts;
+    this.getProducts();
   }
 
   onShowCategory(newCategory: string): void {
